e510e36cc7fc376ff9bcd096f9071cbce4137bb6
a4a881d4 <a4a881d4@163.com>
2013-05-20 00:57:17 +0800

    2e9 test

--- tests/testDelay.sh
+++ tests/testDelay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-work/delayLog 100000000 2000000 > log & echo $! > /sys/fs/cgroup/cpuset/others/tasks
+work/delayLog 2000000000 2000000 > log & echo $! > /sys/fs/cgroup/cpuset/others/tasks
 
-work/multiTask HelloV -r 100000000 -d 2000000 & echo $! > /sys/fs/cgroup/cpuset/others/tasks
+work/multiTask HelloV -r 2000000000 -d 2000000 & echo $! > /sys/fs/cgroup/cpuset/others/tasks
 
